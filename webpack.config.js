2e14d2b55be6efe11719f3509bf311ee4238f3cd
Ekaterina Domashova <katy.domashova@gmail.com>
2025-05-16 22:43:50 +0300

    fix nav position

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,35 @@ export default {
           },
         },
       },
-      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] },
+      // { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.css$/,
+        exclude: /node_modules\/react-hot-toast/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: false,
+            },
+          },
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        include: /node_modules\/react-hot-toast/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+          'postcss-loader',
+        ],
+      },
       {
         test: /\.(woff(2)?|ttf|eot|otf|svg)$/,
         type: 'asset/resource',
